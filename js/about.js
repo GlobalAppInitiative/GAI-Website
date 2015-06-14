fb346335c2214292830767819eaae75cd2f4b3be
patels95 <patels326@gmail.com>
2015-06-14 19:10:52 -0400

    added slick slider to about page.

---
+++ js/about.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Sanam on 6/14/15.
+ */
+
+$(document).ready(function(){
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
